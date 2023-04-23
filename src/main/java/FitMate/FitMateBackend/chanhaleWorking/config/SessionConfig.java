c37446391943e03dfce8a9a6f6c78c7bfd6529ed
chanhale <chan771788@gmail.com>
2023-04-23 16:08:39 +0900

    **ChanhaleWorking** 2023.04.23 16:07
    인터셉터 등록해제
    - 개발단계에선 인터셉터 꺼놓는게 나을것이라 판단하여 구현을 서비스에 적용하지 않음

--- src/main/java/FitMate/FitMateBackend/chanhaleWorking/config/SessionConfig.java
+++ src/main/java/FitMate/FitMateBackend/chanhaleWorking/config/SessionConfig.java
@@ -6,7 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+// 개발단계에선 인터셉터를 꺼놓을 것임.
+//@Configuration
 public class SessionConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {

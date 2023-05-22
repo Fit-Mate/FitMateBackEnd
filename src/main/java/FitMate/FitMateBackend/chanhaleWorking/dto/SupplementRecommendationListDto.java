51b0fc3814a5572b67e29107f8a96448488ef418
chanhale <chan771788@gmail.com>
2023-05-22 20:00:19 +0900

    **ChanhaleWorking** 2023.05.22 19:59
    supplementRecommendationListDto 에 supplementKoreanName 추가
    supplementListDto에 supplementType 추가

--- src/main/java/FitMate/FitMateBackend/chanhaleWorking/dto/SupplementRecommendationListDto.java
+++ src/main/java/FitMate/FitMateBackend/chanhaleWorking/dto/SupplementRecommendationListDto.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Getter
@@ -16,11 +17,15 @@ public class SupplementRecommendationListDto {
     private Long supplementRecommendationId;
     private LocalDate date;
     private String question;
+    private List<String> supplementKoreanName = new ArrayList<>();
     public static SupplementRecommendationListDto createSupplementRecommendationDto(SupplementRecommendation supplementRecommendation) {
         SupplementRecommendationListDto supplementRecommendationListDto = new SupplementRecommendationListDto();
         supplementRecommendationListDto.supplementRecommendationId = supplementRecommendation.getId();
         supplementRecommendationListDto.date = supplementRecommendation.getDate();
         supplementRecommendationListDto.question = supplementRecommendation.getQueryText();
+        for (RecommendedSupplement rs : supplementRecommendation.getRecommendedSupplements()) {
+            supplementRecommendationListDto.supplementKoreanName.add(rs.getSupplement().getKoreanName());
+        }
         return supplementRecommendationListDto;
     }
 }
